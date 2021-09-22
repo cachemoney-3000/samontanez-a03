d88f7e40060e329ae208550834a0522d09ab3076
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-22 16:18:26 -0400

    exercise24 initial commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+
+package baseline;
+/*
+Print "Enter two strings and I'll tell you if they are anagram:"
+    to prompt the user to type an input
+
+Method called firstString will prompt user to type the first input
+    Inside the method firstString:
+        Print "Enter the first string:"
+
+Method called secondString will prompt user to type the second input
+    Inside the method secondString:
+        Print "Enter the second string:"
+
+Method called isAnagram will check if the two strings are anagram or not
+    Inside the isAnagram method:
+        it will compare the first string and second string letter by letter
+ */
+
+public class Solution24 {
+}
