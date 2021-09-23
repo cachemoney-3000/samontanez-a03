c1c36c918073adbafb1f1170a95b4c8042838e9e
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-22 20:43:11 -0400

    Initial commit exercise25

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+
+/*
+Main:
+    PROMPT: "Enter a password:"
+    'password' will store the user input
+    Call the finalOutput to print result.
+
+Method passwordValidator:
+    if (password only consists of numbers and contains less than 8 char)
+        passStrength = very weak password
+    else if(password only consists of letters and fewer than 8 chars)
+        passStrength = weak password
+    else if(password consists of alphanumeric and at least 8 chars)
+        passStrength = strong password
+    else
+        passStrength = very strong password
+
+Method finalOutput:
+    (Will print out the result of passwordValidator)
+    Print: The password <password> is a <passStrength> password.
+
+ */
+
+package baseline;
+
+public class Solution25 {
+}
