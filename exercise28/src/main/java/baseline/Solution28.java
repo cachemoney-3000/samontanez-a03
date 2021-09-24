9994a565e88e1b6a03034fe49d75f8e0375b2b03
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-24 17:41:31 -0400

    initial commit - exercise28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class Solution28 {
+    public static void main(String[] args) {
+        //for loop that loops until 5 numbers was entered
+        //initialize num to zero
+        for(int i = 0; i < 5; i++){
+            //prompts user to enter a number
+            //'num' will store the input
+            //'total' will add the numbers as they were entered by the user
+        }
+        //prints the total 
+    }
+}
+
+
