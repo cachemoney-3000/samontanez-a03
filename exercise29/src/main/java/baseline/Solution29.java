bab8625e7e0da31279a8fd48d0aca8f5c34de6ed
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-24 22:32:22 -0400

    initial commit exercise29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution29 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //prompt user asking for the rate of return
+        //store the input to rateOfReturn
+
+        //if the user enter 0, don't allow
+            //print "input not allowed"
+            //ask again
+
+        //if the user enter a non-numeric value, ask again
+            //print "input not allowed"
+            //ask again
+
+        //
+    }
+
+    //a method that will handles the input, and do not accept bad inputs
+
+    //output method will print out the years to double an investment
+    //uses the formula 'years' = 72/r
+}
