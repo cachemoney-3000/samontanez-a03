d3c0d652ff8eae3f44b8b133b25ab960c4931b72
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-25 14:34:54 -0400

    updated exercise30: comments and code

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -30,7 +30,23 @@ public class Solution30 {
             System.out.println();
         }
     }
-
-
 }
+
+class test {
+    //Method for unit testing. The code is based on the multiplicationTable method
+    public static int multiplicationTableTest(int i, int j) {
+        //used an array to know the value when multiplying 2 numbers between 1-12 inclusive
+        int[][] multiply = new int[13][13];
+        //the outer loop will loop from 1 to 12
+        for (int a = 1; a <= i; a++) {
+            //inner loop will also loop up to 12
+            for (int b = 1; b <= j; b++) {
+                //multiply i*j
+                multiply[a][b] = a*b;
+            }
+        }
+        return multiply[i][j];
+    }
+}
+
 
