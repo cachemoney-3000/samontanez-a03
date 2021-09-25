634c2eb3b46d824c08a2c99fa90651a39d99fa3c
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-25 16:40:30 -0400

    exercise32 initial commit

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution32 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //header
+        System.out.println("Let's play Guess the Number!");
+        //prompt user asking for the difficulty level (1-3)
+        //prompt user guessing a number
+            //will keep prompting user until the guess is correct
+
+        //prompt user if they want to play again
+    }
+
+    //method that will deal with the guesses, counts how many guess until correct
+    public void guess(){
+
+    }
+
+    //take care of the difficulty level of the game, and the inputs
+    private void difficultyLevel(){
+
+    }
+
+    private void playAgain(){
+
+    }
+}
