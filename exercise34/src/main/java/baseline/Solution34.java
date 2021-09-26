55f84737582cc35cee58293f6678b27e9cc136b2
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-26 01:51:46 -0400

    initial commit exercise 34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+package baseline;
+import java.util.Scanner;
+
+/*
+* Create a small program that contains a list of employee names.
+* Print out the list of names when the program runs the first time.
+* Prompt for an employee name and remove that specific name from the list of names.
+* Display the remaining employees, each on its own line.
+ */
+
+public class Solution34 {
+    //array to store the name of employees
+    private String[] employees;
+    private int totalEmployees;
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    //constructor to store the list of names
+    public Solution34(){
+    }
+
+    public static void main(String[] args) {
+        //prints the list of names
+        //prompts user asking for a name to be removed
+        //store the input to String 'removeEmployeeStr'
+
+        //call the method that will remove the name
+
+    }
+
+    //method that will remove the name of an employee
+    public void removeEmployeeName(){
+
+    }
+}
