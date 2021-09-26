ff453ef15cc815b8e1f1f66d935ea887520aa65a
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-25 23:40:28 -0400

    initial commit exercise33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+package baseline;
+import java.util.Scanner;
+
+/*
+* Create a Magic 8 Ball game that
+* Prompts for a question
+* Then displays either "Yes," "No," "Maybe," or "Ask again later.
+*/
+
+public class Solution33 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //prompts user asking them to ask a question
+        //user types the question
+
+        //program will call the answer method to print out a random response
+    }
+
+    //method that will takes care of random answer (yes, no, ask again)
+    public String answer(){
+        //store the possible answer to an array
+        //have the random generator choose which answer to choose
+
+    }
+
+}
