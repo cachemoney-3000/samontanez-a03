8030ddecaa9869331164c841cd43a42fa3b50132
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-28 23:24:38 -0400

    updated exercise33

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -16,22 +16,24 @@ public class Solution33 {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Solution33 sol33 = new Solution33();
         //prompts user asking them to ask a question
         System.out.println("What's your question? ");
         scanner.nextLine();
         //program will call the answer method to print out a random response
-        System.out.println(sol33.answer());
+        System.out.println(answer());
     }
 
     //method that will takes care of random answer ex:(yes, no, maybe, ask again)
-    public String answer(){
+    public static String answer(){
         //store the possible answer to an array
         String[] response = {"Yaaaaaass", "Hell nah!", "Sheesh! Who knows", "Ask again later", "Gods plan", "Yeeet"};
+
         //random number generator
         Random rand = new Random();
+
         //the responseIndex creates a random numbers based on how many Strings are stored in 'response'
         int responseIndex = rand.nextInt(response.length);
+
         //prints out a random response
         return (response[responseIndex]);
     }
