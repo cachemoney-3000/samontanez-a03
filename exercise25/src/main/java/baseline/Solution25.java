354256ff78f7f4bcdd17ba76b80e2452fc52cf33
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-28 21:20:15 -0400

    updated comments

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -3,6 +3,14 @@
  *  Copyright 2021 Joshua Samontanez
  */
 package baseline;
+/*
+* Create a program that determines the complexity of a given password based on these rules:
+    * A very weak password contains only numbers and is fewer than eight characters.
+    * A weak password contains only letters and is fewer than eight characters.
+    * A strong password contains letters and at least one number and is at least eight characters.
+    * A very strong password contains letters, numbers, and special characters and is at least eight characters.
+    * If a password does not meet any of these rules, then report it as a password of unknown strength.
+ */
 
 import java.util.Scanner;
 
